2e2d7366ad21e248bd613638e31ce249eedc51b5
bcgsnowflake <107079541+bcgsnowflake@users.noreply.github.com>
2022-06-08 09:33:56 -0500

    Update streamlit_app.py
    added .loc

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,8 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 
 # lets put in a pick list here where customers can pick the fruit they want to include
-streamlit.multiselect("Pick some Fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_selected = streamlit.multiselect("Pick some Fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # display the table on the page
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
